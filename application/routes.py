b88bf293e5330f3eb6276b033fde0e0b8b8a5557
nathangiose <nathangiose@gmail.com>
2021-08-05 00:30:09 +0200

    changing dictionary into an api-start 00:29 05-aug-2021

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,8 @@
 from application import app
-from flask import render_template, request
+from flask import render_template, request, json, Response
+
+courseData = [{"courseID": "1111", "title": "PHP 101", "description": "Intro to PHP", "credits": 3, "term": "Fall, Spring"}, {"courseID": "2222", "title": "Java 1", "description": "Intro to Java Programming", "credits": 4, "term": "Spring"}, {"courseID": "3333", "title": "Adv PHP 201",
+                                                                                                                                                                                                                                                   "description": "Advanced PHP Programming", "credits": 3, "term": "Fall"}, {"courseID": "4444", "title": "Angular 1", "description": "Intro to Angular", "credits": 3, "term": "Fall, Spring"}, {"courseID": "5555", "title": "Java 2", "description": "Advanced Java Programming", "credits": 4, "term": "Fall"}]
 
 
 @app.route("/")
@@ -17,9 +20,7 @@ def login():
 @app.route("/courses/")
 @app.route("/courses/<term>")
 def courses(term="Winter 2021"):
-    courseData = [{"courseID": "1111", "title": "PHP 101", "description": "Intro to PHP", "credits": 3, "term": "Fall, Spring"}, {"courseID": "2222", "title": "Java 1", "description": "Intro to Java Programming", "credits": 4, "term": "Spring"}, {"courseID": "3333", "title": "Adv PHP 201",
-                                                                                                                                                                                                                                                       "description": "Advanced PHP Programming", "credits": 3, "term": "Fall"}, {"courseID": "4444", "title": "Angular 1", "description": "Intro to Angular", "credits": 3, "term": "Fall, Spring"}, {"courseID": "5555", "title": "Java 2", "description": "Advanced Java Programming", "credits": 4, "term": "Fall"}]
-    print(courseData[0]["title"])
+
     return render_template("courses.html", courseData=courseData, login=True, term=term)
 
 
@@ -34,3 +35,14 @@ def enrollment():
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+
+@app.route("/api/")
+@app.route("/api/<idx>")
+def api(idx=None):
+    if(idx == None):
+        jdata = courseData
+    else:
+        jdata = courseData[int(idx)]
+
+    return Response(json.dumps(jdata), mimetype="application/json")
